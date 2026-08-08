2112149107714f45f921c490f4d555ca4e06afce
bbednarski9 <bbednarski9@users.noreply.github.com>
2026-08-08 16:23:44 +0000

    Deploy preview for PR 2 🛫

---
+++ pr-preview/pr-2/rust/trait.impl/switchyard_protocol/client/trait.Decision.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["switchyard_libsy",[["impl <a class=\"trait\" href=\"switchyard_protocol/client/trait.Decision.html\" title=\"trait switchyard_protocol::client::Decision\">Decision</a> for <a class=\"struct\" href=\"switchyard_libsy/struct.FallThroughDecision.html\" title=\"struct switchyard_libsy::FallThroughDecision\">FallThroughDecision</a>",0],["impl <a class=\"trait\" href=\"switchyard_protocol/client/trait.Decision.html\" title=\"trait switchyard_protocol::client::Decision\">Decision</a> for <a class=\"struct\" href=\"switchyard_libsy/struct.NoopDecision.html\" title=\"struct switchyard_libsy::NoopDecision\">NoopDecision</a>",0],["impl <a class=\"trait\" href=\"switchyard_protocol/client/trait.Decision.html\" title=\"trait switchyard_protocol::client::Decision\">Decision</a> for <a class=\"struct\" href=\"switchyard_libsy/struct.PassthroughDecision.html\" title=\"struct switchyard_libsy::PassthroughDecision\">PassthroughDecision</a>",0]]],["switchyard_protocol",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[943,27]}
